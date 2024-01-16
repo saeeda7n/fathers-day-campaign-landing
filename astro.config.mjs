0ec8261f073ae91a735eebb74af6212024f021fb
saeed1202 <saeed1202@users.noreply.github.com>
2024-01-17 02:43:39 +0330

    5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), relativeLinks()]
 });
