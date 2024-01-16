590243be19b6500ba154832848fad25d2c478f08
saeed1202 <saeed1202@users.noreply.github.com>
2024-01-16 20:12:44 +0330

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1400px"
+      }
+    },
+    extend: {
+      fontFamily: {
+        kalameh: ["KalamehWeb", "Tahoma", "sens-serif"]
+      },
+      colors: {
+        primary: "#EF3824",
+        "cetacean-blue": "#090830"
+      }
+    }
+  },
+  plugins: []
+};
